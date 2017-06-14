3b2bb16e1687329f6dc41b19e97336f21b56ed24
Matt Catha <mattcatha@gmail.com>
2017-06-14 15:59:51 -0500

    Create index.js

--- js/index.js
+++ js/index.js
@@ -3,11 +3,6 @@ var getWeatherMyCity = function() {
 	
 	$("#weather").remove();
 	
-	jQuery.ajaxPrefilter(function(options) {
-        	if (options.crossDomain && jQuery.support.cors) {
-            		options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-        	}
-    	});
 	// Call location API
 	$.ajax({
 		method: "GET",
